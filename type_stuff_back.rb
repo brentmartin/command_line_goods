2019e205d3b73ac873f51764a198bb4a0f208442
brentmartin <bmart35@gmail.com>
2017-08-02 23:57:21 -0500

    refactoring

--- type_stuff_back.rb
+++ type_stuff_back.rb
@@ -6,69 +6,42 @@ def print_out(length, array, speed)
     i += 1
     system('clear')
     text = array[0..i].join("")
-    print text
+    puts text
     sleep speed
   end
 end
 
 def type_stuff
-  sleep 0.5
-  if @first_time == false
-    puts "Something else? Type something or press (s) to set speed."
-    print "> "
-    @input = gets.chomp.to_s
-    if @input.upcase == "S" then set_speed end
-  end
-  unless @first_time == false
-    puts "Type something here, and i'll type it back to you!"
-    print "> "
-    @input = gets.chomp.to_s
-    @first_time = false
-  end
+  if @first_time == false then @first_time = false; print "Type something here, and i'll type it back to you!\n> "
+  else print "Something else? Type something or press (s) to set speed. \n> " end
+  @input = gets.chomp.to_s
+  if @input.upcase == "S" then set_speed end
   body = @input.split("")
   speed = @speed.to_f
   system('clear')
   sleep 0.5
   print_out(body.length, body, speed)
   sleep 1
-  puts ""
-  puts ""
-  go
+  type_stuff
 end
 
 def set_speed
   system('clear')
-  puts "What speed would you like me to type back? (enter a number)"
-  puts "1) chillin'"
-  puts "2) fast"
-  puts "3) smoking!"
-  print "> "
+  print "What speed would you like me to type back? (enter a number)\n1) chillin\n2) fast\n3) smoking!\n> "
   speed_setting = gets.chomp.to_i
-  case speed_setting
-  when 1
-    puts "Aight, we can do this slow"
-    @speed = 0.1
-    sleep 1
-  when 2
-    puts "You like fast huh?"
-    @speed = 0.05
-    sleep 1
-  when 3
-    puts "Hold on to your keyboard!"
-    @speed = 0.02
-    sleep 1
-  else
-    "I'm sorry, can you try that again?"
-  end
   system('clear')
+  case speed_setting
+  when 1 then @speed = 0.1
+    puts "Aight, we can do this slow"
+  when 2 then @speed = 0.05
+    puts "You like fast huh?"
+  when 3 then @speed = 0.02
+    puts "Hold on to your keyboard!"
+  else "I'm sorry, can you try that again?"
+  end
+  sleep 1
   type_stuff
-end
-
-def go
-  type_stuff
-  set speed
 end
 
 system('clear')
-sleep 0.5
-go
+type_stuff
